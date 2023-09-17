a14c5676bff1af87f0109f5b08289c525787bcd4
umairahmad3738 <umairahmad3738@gmail.com>
2023-09-17 15:09:20 +0500

    Moving file

---
+++ ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using kudvenkitPractice.Models;
+
+namespace kudvenkitPractice.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+        public string Name { get; set; }
+        [Required]
+        public GenderEnums? Gender { get; set; }
+        [Required]
+        public DepartmentEnums? Department { get; set; }
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$", ErrorMessage = "Invalid Email")]
+        [Display(Name = "Office Email")]
+        public string Email { get; set; }
+        public IFormFile Photo { get; set; }
+
+
+    }
+}
